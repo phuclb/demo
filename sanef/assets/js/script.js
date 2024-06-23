f21295d43bee8b658f484608bcbbd0f5da4b1fd7
Augustin Le <lbphuc1108@gmail.com>
2024-06-23 11:01:07 +0700

    Sanef

--- sanef/assets/js/script.js
+++ sanef/assets/js/script.js
@@ -15,17 +15,35 @@
      * Item
      */
     fNItem: () => {
+      const wrapper = document.querySelector('.wrapper');
       const itemX = document.querySelectorAll('.itemX');
       const itemXTotal = itemX.length;
-      const itemXTemp = new Array(itemXTotal).fill(0);
-      const scrollbarToggle = (isScrollable) => {
-        const viewport = document.querySelector('html');
-        viewport.classList.toggle('of--hidden', isScrollable);
-      };
       if (itemXTotal) {
+        const itemXDelay = 650;
+        const itemXTemp = new Array(itemXTotal).fill(0);
+
+        // Scrollbar
+        const scrollbarToggle = (isScrollable) => {
+          const viewport = document.querySelector('html');
+          viewport.classList.toggle('of--hidden', isScrollable);
+        };
+
+        // Result
+        const itemXResult = (itemId) => {
+          if (itemId === itemXTotal - 1) {
+            let wrapperH = 0;
+            itemXTemp.forEach(itemH => wrapperH += itemH);
+            wrapper.style.removeProperty('height');
+            wrapperH += wrapper.getBoundingClientRect().height;
+            wrapper.style.height = wrapperH + 'px';
+          }
+        };
+
+        // Item
         itemX.forEach((itemY, itemYId) => {
           const isSplide = itemY.classList.contains('splideX');
           const isFigure = itemY.classList.contains('figureX');
+          const stickyX = itemY.querySelector('.stickyX');
 
           // Splide
           if (isSplide) {
@@ -36,48 +54,7 @@
             const splideSlide = splideY.querySelector('.splide__slide');
             let splideGap, splidePerPage, splideInTotal, splideSlideW;
             let splideYEnd, splideYStart, splideYTop;
-            let scrollPos = 0;
-
-            // Handle
-            const splideHandle = () => {
-              let splideMT = 0;
-              let splideHeight, splideTransform;
-              splideHeight = Math.floor(splideDiv.getBoundingClientRect().height) + splideMT;
-              splideTransform = -splideMT;
-              splideY.style.height = splideHeight + 'px';
-              splideList.style.transform = 'translateX(' + splideTransform + 'px)';
-              splide.go(0);
-              scrollbarToggle(true);
-              window.scrollTo(0, 0);
-
-              splideGap = splide.options.gap;
-              splidePerPage = splide.options.perPage;
-              splideInTotal = splide.length;
-              splideSlideW = Math.floor(splideSlide.getBoundingClientRect().width);
-              splideYTop = Math.floor(splideY.offsetTop);
-
-              let splideXTemp = 0;
-              itemXTemp.forEach((item, index) => {
-                if (index < splideYId) {
-                  splideXTemp += item;
-                }
-              });
-              if (splideYId === 0) {
-                itemXTemp[splideYId] = 0;
-              }
-              splideYEnd = 0;
-              if (window.innerWidth >= 768) {
-                splideYEnd = (splideSlideW + splideGap) * (splideInTotal - splidePerPage);
-              }
-              splideYStart = splideXTemp + splideYTop;
-              itemXTemp[splideYId] = splideYEnd;
-              if (splideYId === itemXTotal - 1) {
-                itemXTemp[splideYId] = 0;
-                scrollbarToggle(false);
-              }
-
-              console.log('Splide ' + splideYId + ': ' + splideYStart);
-            };
+            let splideTimer = false;
 
             // Splide
             const splide = new Splide(splideDiv, {
@@ -102,10 +79,47 @@
                 }
               }
             });
-            splide.on('ready resized', () => {
-              splideHandle();
-            });
             splide.mount();
+
+            // Handle
+            const splideHandle = () => {
+              let splideMT = 0;
+              let splideHeight, splideTransform;
+              splideHeight = Math.floor(stickyX.getBoundingClientRect().height) + splideMT;
+              splideTransform = -splideMT;
+              splideY.style.height = splideHeight + 'px';
+              splideList.style.transform = 'translateX(' + splideTransform + 'px)';
+              splide.go(0);
+              window.scrollTo(0, 0);
+              scrollbarToggle(true);
+
+              splideGap = splide.options.gap;
+              splidePerPage = splide.options.perPage;
+              splideInTotal = splide.length;
+              splideSlideW = Math.floor(splideSlide.getBoundingClientRect().width);
+              splideYTop = Math.floor(splideY.offsetTop);
+
+              let splideXTemp = 0;
+              itemXTemp.forEach((item, index) => {
+                if (index < splideYId) {
+                  splideXTemp += item;
+                }
+              });
+              if (splideYId === 0) {
+                itemXTemp[splideYId] = 0;
+              }
+              splideYEnd = 0;
+              if (window.innerWidth >= 768) {
+                splideYEnd = (splideSlideW + splideGap) * (splideInTotal - splidePerPage);
+              }
+              splideYStart = splideXTemp + splideYTop;
+              itemXTemp[splideYId] = splideYEnd;
+              if (splideYId === itemXTotal - 1) {
+                scrollbarToggle(false);
+              }
+
+              console.log(`Splide ${splideYId}: Start ${splideYStart} / End ${splideYEnd}`);
+            };
 
             // Control
             const splideControl = (scrollPos) => {
@@ -118,13 +132,27 @@
                   splideMT = splideYEnd;
                 }
               }
-              splideHeight = Math.floor(splideDiv.getBoundingClientRect().height) + splideMT;
+              splideHeight = Math.floor(stickyX.getBoundingClientRect().height) + splideMT;
               splideTransform = -splideMT;
               splideY.style.height = splideHeight + 'px';
               splideList.style.transform = 'translateX(' + splideTransform + 'px)';
             };
+
+            // Init
+            splideHandle();
+            itemXResult(itemYId);
+
+            // Resize
+            window.addEventListener('resize', () => {
+              clearTimeout(splideTimer);
+              splideTimer = setTimeout(() => {
+                splideHandle();
+                itemXResult(splideYId);
+              }, itemXDelay);
+            });
   
             // Scroll
+            let scrollPos = 0;
             window.addEventListener('scroll', () => {
               scrollPos = window.scrollY;
               window.requestAnimationFrame(() => {
@@ -139,17 +167,17 @@
             const figureYId = itemYId;
             const figureMedia = figureY.querySelectorAll('.block-media');
             const figureTotal = figureMedia.length - 1;
-            const figureDelay = 750;
             let figureYEnd, figureYStart;
             let figureTimer = false;
-            let scrollPos = 0;
 
             // Handle
             const figureHandle = () => {
               let figureMT = 0;
-              figureY.style.marginTop = figureMT + 'px';
-              scrollbarToggle(true);
+              let figureHeight;
+              figureHeight = Math.floor(stickyX.getBoundingClientRect().height) + figureMT;
+              figureY.style.height = figureHeight + 'px';
               window.scrollTo(0, 0);
+              scrollbarToggle(true);
 
               let figureXTemp = 0;
               itemXTemp.forEach((item, index) => {
@@ -159,41 +187,42 @@
               });
 
               const figureYT = Math.floor(figureY.offsetTop);
-              const figureYH = Math.floor(figureY.getBoundingClientRect().height);
+              const stickyXH = Math.floor(figureY.getBoundingClientRect().height);
   
               if (figureYId === 0) {
                 itemXTemp[figureYId] = 0;
               }
-              figureYEnd = figureYH * figureTotal;
+              figureYEnd = stickyXH * figureTotal;
               figureYStart = figureXTemp + figureYT;
               itemXTemp[figureYId] = figureYEnd;
               if (figureYId === itemXTotal - 1) {
-                itemXTemp[figureYId] = 0;
                 scrollbarToggle(false);
               }
 
-              console.log('Figure ' + figureYId + ': ' + figureYStart);
+              console.log(`Figure ${figureYId}: Start ${figureYStart} / End ${figureYEnd}`);
             };
   
             // Control
             const figureControl = (scrollPos) => {
               let figureMT = 0;
               let figureId = 0;
-              const figureYH = Math.floor(figureY.getBoundingClientRect().height);
+              let figureHeight;
+              const stickyXH = Math.floor(stickyX.getBoundingClientRect().height);
               if (scrollPos >= figureYStart) {
                 if (scrollPos < (figureYEnd + figureYStart)) {
                   figureMT = scrollPos - figureYStart;
                 } else {
-                  figureMT = figureYEnd - 1;
+                  figureMT = figureYEnd;
                 }
-                figureId = Math.floor((figureMT + figureYH) / figureYH);
+                figureId = Math.floor((figureMT + stickyXH) / stickyXH);
               }
-              figureY.style.marginTop = figureMT + 'px';
+              figureHeight = stickyXH + figureMT;
+              figureY.style.height = figureHeight + 'px';
               figureMedia.forEach((media, index) => {
                 let opaque = 1;
                 if (index > 0) {
                   if (index === figureId) {
-                    opaque = (figureMT / figureYH) - Math.floor(figureMT / figureYH);
+                    opaque = (figureMT / stickyXH) - Math.floor(figureMT / stickyXH);
                   } else {
                     opaque = index < figureId ? 1 : 0;
                   }
@@ -201,15 +230,22 @@
                 media.style.opacity = opaque;
               });
             };
+  
+            // Init
+            figureHandle();
+            itemXResult(itemYId);
 
             // Resize
-            figureHandle();
             window.addEventListener('resize', () => {
               clearTimeout(figureTimer);
-              figureTimer = setTimeout(figureHandle, figureDelay);
+              figureTimer = setTimeout(() => {
+                figureHandle();
+                itemXResult(figureYId);
+              }, itemXDelay);
             });
   
             // Scroll
+            let scrollPos = 0;
             window.addEventListener('scroll', () => {
               scrollPos = window.scrollY;
               window.requestAnimationFrame(() => {
