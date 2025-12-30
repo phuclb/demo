ef3c1fa94eb94c1370ea8d420215cce26941a0ce
Augustin Le <lbphuc1108@gmail.com>
2025-12-30 10:08:32 +0700

    Litrimarche

--- litrimarche/assets/js/script.js
+++ litrimarche/assets/js/script.js
@@ -6,6 +6,7 @@ const App = {
    */
   init() {
     App.splide();
+    App.toggle();
   },
 
   /**
@@ -21,9 +22,8 @@ const App = {
         arrows: false,
         autoplay: true,
         interval: 5000,
-        reducedMotion: {
-          speed: 0,
-          autoplay: false
+        classes: {
+          pagination: 'splide__pagination justify-center'
         }
       }).mount();
     }
@@ -34,14 +34,86 @@ const App = {
         type: 'fade',
         speed: 750,
         gap: 0,
-        pagination: false,
+        pagination: false
+      }).mount();
+    }
+
+    let productClass = '.splide-product', productSplide = document.querySelector(productClass);
+    if (productSplide) {
+      productSplide = new Splide(productClass, {
+        type: 'slide',
+        speed: 750,
+        gap: 26,
+        perPage: 4,
+        perMove: 1,
+        pagination: false
+      }).mount();
+    }
+
+    let priorityClass = '.splide-priority', prioritySplide = document.querySelector(priorityClass);
+    if (prioritySplide) {
+      prioritySplide = new Splide(priorityClass, {
+        type: 'slide',
+        speed: 750,
+        gap: 16,
+        autoWidth: true,
+        arrows: false,
         autoplay: true,
         interval: 5000,
-        reducedMotion: {
-          speed: 0,
-          autoplay: false
+        classes: {
+          pagination: 'splide__pagination justify-start'
         }
       }).mount();
+    }
+  },
+
+  /**
+   * Toggle
+   */
+  toggle() {
+    const toggles = document.querySelectorAll('.toggle');
+    if (toggles.length) {
+      toggles.forEach(toggle => {
+        const target = document.getElementById(toggle.getAttribute('aria-controls'));
+        if (target) {
+          toggle.addEventListener('click', (e) => {
+            const isActive = toggle.classList.contains('active');
+            const activeToggles = document.querySelectorAll('.toggle.active');
+            if (!isActive && activeToggles.length) {
+              activeToggles.forEach(activeToggle => {
+                const activeTarget = document.getElementById(activeToggle.getAttribute('aria-controls'));
+                if (!activeTarget.contains(toggle)) {
+                  activeTarget.classList.remove('active');
+                  activeToggle.classList.remove('active');
+                  activeToggle.setAttribute('aria-expanded', isActive);
+                }
+              });
+            }
+            target.classList.toggle('active');
+            toggle.classList.toggle('active');
+            toggle.setAttribute('aria-expanded', !isActive);
+            e.preventDefault();
+          });
+        }
+      });
+
+      document.addEventListener('click', (e) => {
+        const target = e.target;
+        if (!target.closest('.active')) {
+          App.toggleOff();
+        }
+      });
+    }
+  },
+  toggleOff() {
+    const activeToggles = document.querySelectorAll('.toggle.active');
+    if (activeToggles.length) {
+      activeToggles.forEach(activeToggle => {
+        const activeTarget = document.getElementById(activeToggle.getAttribute('aria-controls'));
+        activeTarget.classList.remove('active');
+        activeToggle.classList.remove('active');
+        activeToggle.setAttribute('aria-expanded', false);
+      });
     }
   },
 
