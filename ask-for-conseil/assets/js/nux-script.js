429fb80b808c113e123de34c2ec530cd3d3ddfd2
Augustin Le <lbphuc1108@gmail.com>
2026-03-24 20:18:16 +0700

    Ask4Conseil

--- ask-for-conseil/assets/js/nux-script.js
+++ ask-for-conseil/assets/js/nux-script.js
@@ -5,13 +5,65 @@ const App = {
    * Initialize the application
    */
   init() {
-    App.fN();
+    App.card();
+    App.slide();
+    App.misc();
   },
 
   /**
-   * Placeholder for a function
+   * Card
    */
-  fN() {
+  card() {
+  },
+
+  /**
+   * Slide
+   */
+  slide() {
+    const createSplide = (selector, options) => {
+      document.querySelectorAll(selector).forEach(el => {
+        let splide = new Splide(el, options);
+
+        // element
+        const index = el.querySelector('.splide__index');
+        const total = el.querySelector('.splide__total');
+
+        // event
+        splide.on('ready', () => {
+          index.textContent = splide.index + 1;
+          total.textContent = splide.length;
+        }).on('moved', () => {
+          index.textContent = splide.index + 1;
+        });
+
+        // mount
+        splide.mount();
+      });
+    };
+
+    createSplide('.splide-card', {
+      type: 'slide',
+      gap: 20,
+      speed: 1000,
+      focus: 0,
+      autoWidth: true,
+      pagination: false,
+      omitEnd: true
+    });
+  },
+
+  /**
+   * Misc
+   */
+  misc() {
+    document.body.matchHeight({
+      elements: null,
+      byRow: true,
+      property: 'min-height',
+      attributeName: 'data-mh',
+      events: true,
+      throttle: 80
+    });
   }
 };
 
